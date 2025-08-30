2601af7af5b6f1daaf5475e4f8dcd209e9084833
Akins-Coded <akindipemuheez@gmail.com>
2025-08-30 08:34:32 +0100

    cache_metrics

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,10 @@
+import logging
+from django_redis import get_redis_connection
 from django.core.cache import cache
 from .models import Property
+
+
+logger = logging.getlogger(__nme__)
 
 def get_all_properties():
     # Try To get from cache
@@ -12,3 +17,25 @@ def get_all_properties():
         cache.set('all_properties', properties, timeout=3600)  # Cache for 1 hour
 
     return properties
+
+def get_redis_cache_metrics():
+    """
+    Retrive Redis cache metrics: hits, misses and hit ratio
+    """
+    conn = get_redis_connection("default")
+    info = conn.info("stats") # get Redis stats
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses", 0)
+
+    total = hits + misses
+    hit_ratio = (hits / total) if total > 0 else 0
+
+    metrics = {
+        "hits": hits,
+        "misses": misses,
+        "hit_ratio": round(hit_ratio, 2)
+    }
+
+    logger.info(f"Redis Cache Metrics: {metrics}")
+    return metrics
--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
-from .utils import get_all_properties
+from .utils import get_all_properties, get_redis_cache_metrics
 
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = get_all_properties()
     return JsonResponse({"data": properties})
 
+def cache_metrics(request):
+    metrics = get_redis_cache_metrics()
+    return JsonResponse(metrics)
